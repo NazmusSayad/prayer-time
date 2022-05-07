7b0a9384f90dc31d318ff9cd7702981ee11240ef
Nazmus Sayad <247sayad@gmail.com>
2022-05-08 00:03:16 +0600

    localstroage

--- script/main.js
+++ script/main.js
@@ -19,10 +19,10 @@ const config = {
   },
   extraTime_name: "",
   coordinates: {
-    latt: localStorage.getItem("latt") ? localStorage.getItem("latt") : null,
-    long: localStorage.getItem("long") ? localStorage.getItem("long") : null,
+    latt: localStorage.getItem("latt"),
+    long: localStorage.getItem("long"),
   },
-  method: localStorage.getItem("method") ? localStorage.getItem("method") : "Karachi",
-  madhab: localStorage.getItem("madhab") ? localStorage.getItem("madhab") : "Hanafi",
+  method: localStorage.getItem("method") || "Karachi",
+  madhab: localStorage.getItem("madhab") || "Hanafi",
   fajrLoaded: false,
 };
