998aec772b2b955c4ebddf757c9d435d1933701c
Nazmus Sayad <247sayad@gmail.com>
2022-06-06 23:40:54 +0600

    indent fixed

--- script/app.js
+++ script/app.js
@@ -1,33 +1,33 @@
 ;(() => {
-  if (config.coordinates.latt && config.coordinates.long) update_time_database() // If coordinates are already exists
-  else {
-    // basically when first time using
-    update_coordinates_ip()
+   if (config.coordinates.latt && config.coordinates.long) update_time_database() // If coordinates are already exists
+   else {
+      // basically when first time using
+      update_coordinates_ip()
 
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        //If Geolocation works
-        update_coordinates_config(position.coords.latitude, position.coords.longitude)
-      })
-    }
-  }
+      if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition((position) => {
+            //If Geolocation works
+            update_coordinates_config(position.coords.latitude, position.coords.longitude)
+         })
+      }
+   }
 })()
 
 window.addEventListener("dblclick", fullscreenFn)
 
 window.addEventListener("click", (event) => {
-  event.ctrlKey && element.documentHTML.classList.toggle("cursorHide")
+   event.ctrlKey && element.documentHTML.classList.toggle("cursorHide")
 })
 
 window.addEventListener("resize", () => {
-  if (window.innerHeight === screen.height) {
-    element.documentHTML.classList.add("cursorHide")
-  } else {
-    element.documentHTML.classList.remove("cursorHide")
-  }
+   if (window.innerHeight === screen.height) {
+      element.documentHTML.classList.add("cursorHide")
+   } else {
+      element.documentHTML.classList.remove("cursorHide")
+   }
 })
 
 window.addEventListener("contextmenu", (event) => {
-  event.preventDefault()
-  sidebar.show()
+   event.preventDefault()
+   sidebar.show()
 })
