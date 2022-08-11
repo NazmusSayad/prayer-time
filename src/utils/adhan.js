f34ca132e68fcfc8b3a8094ddffb5f682ab6e468
Nazmus Sayad <247sayad@gmail.com>
2022-08-11 23:25:29 +0600

    dhur bal!

--- src/utils/adhan.js
+++ src/utils/adhan.js
@@ -38,32 +38,14 @@ export default (prayer, time) => {
       body: 'Starting time: ' + convertFrom24To12Format(time),
       vibrate: [200, 100, 200],
       requireInteraction: false,
-      icon: './icon_512x512.png',
-      // image: './icon_512x512.png',
-      badge: './icon_512x512.png',
+      badge: 'icon_512x512.png',
+      icon: 'adhan.webp',
     }
   )
-  setTimeout(() => noti.close(), 5000)
+  setTimeout(() => noti.close(), 10000)
 }
 
 document.addEventListener('pointerdown', requestNotificationPermission)
 document.addEventListener('mousedown', requestNotificationPermission)
 document.addEventListener('touchstart', requestNotificationPermission)
 document.addEventListener('keydown', requestNotificationPermission)
-
-document.addEventListener('pointerdown', () => {
-  const prayer = 'Fajr'
-  const time = new Date()
-
-  const noti = new Notification(
-    `It's time for "${prayer.capitalize()}" prayer!`,
-    {
-      body: 'Starting time: ' + convertFrom24To12Format(time),
-      vibrate: [200, 100, 200],
-      requireInteraction: false,
-      badge: 'icon_512x512.png',
-      icon: 'adhan.webp',
-    }
-  )
-  setTimeout(() => noti.close(), 10000)
-})
